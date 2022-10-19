cf821aab449210fda6e4818e0736e210fe770aeb
moon-john <moon-john@naver.com>
2022-10-19 21:18:18 +0900

    [-] dummy code.

--- 7568_덩치.c
+++ 7568_덩치.c
@@ -4,32 +4,25 @@
 int main() {
 	
 	int size, i, j;
-	// 입력
 	scanf("%d", &size);
 	int* kgArr = malloc(sizeof(int) * size);
 	int* cmArr = malloc(sizeof(int) * size);
-	int* rank  = malloc(sizeof(int) * size);
 	
 	for(i = 0; i < size; i++) {
 	   scanf("%d", &kgArr[i]);
 	   scanf("%d", &cmArr[i]);
-	   rank[i] = 1;
 	}
 	
-	for(i = 0; i < size; i++)
-		for(j = 0; j < size; j++) {
-			if (i == j) continue;
-			if (kgArr[i] < kgArr[j] && cmArr[i] < cmArr[j]) rank[i]++;
+	for(i = 0; i < size; i++) {
+		int result = 1;
+		for(j = 0; j < size; j++) { 
+			if (i == j) { continue; }
+			if (kgArr[i] < kgArr[j] && cmArr[i] < cmArr[j]) { result++; }
 		}
-	
-	 
-	// 결과 출력 
-	for(i = 0; i < size - 1; i++)
-		printf("%d ", rank[i]);
-	printf("%d", rank[i]);
+		printf("%d ", result);
+	}
 	
 	free(kgArr);
 	free(cmArr);
-	free(rank);
 }
 
