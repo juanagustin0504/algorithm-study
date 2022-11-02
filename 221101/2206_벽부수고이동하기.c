8f3564dc2f970378c318a9401075f575aa0e50f3
moon-john <moon-john@naver.com>
2022-11-02 20:05:42 +0900

    [+] 7th BFS Algorithm

--- 221101/2206_벽부수고이동하기.c
+++ 221101/2206_벽부수고이동하기.c
@@ -8,11 +8,7 @@ int isArrived = 0;
 int resultLength = 0;
 
 typedef struct Node {
-	int x;
-	int y;
-	int length;
-	int breakingX;
-	int breakingY;
+	int *data;
 	struct Node *next;
 }Node;
 
@@ -31,13 +27,9 @@ int isEmpty(Queue *queue) {
 	return queue->count == 0;
 }
 
-void enqueue(Queue *queue, int y, int x, int length, int breakingY, int breakingX) {
+void enqueue(Queue *queue, int *data) {
 	Node *newNode = (Node *) malloc(sizeof(Node)); // Create New Node
-	newNode->y = y;
-	newNode->x = x;
-	newNode->length = length;
-	newNode->breakingY = breakingY;
-	newNode->breakingX = breakingX;
+	newNode->data = data;
 	newNode->next = NULL;
 	
 	if(isEmpty(queue)) { // Empty Queue
@@ -50,44 +42,40 @@ void enqueue(Queue *queue, int y, int x, int length, int breakingY, int breaking
 }
 
 int *dequeue(Queue *queue) {
+	int *data;
 	Node *ptr;
 	if(isEmpty(queue)) { // Queue is Empty
 		return 0;
 	}
 	
 	ptr = queue->front; // Get front Node
-	int y = ptr->y;
-	int x = ptr->x;
-	int length = ptr->length;
-	int breakingY = ptr->breakingY;
-	int breakingX = ptr->breakingX;
+	data = ptr->data; // Get data
 	queue->front = ptr->next; // Set the next node to front
 	free(ptr);
 	queue->count--;
 	
-	int data[5] = {y, x, length, breakingY, breakingX};
-	
 	return data;
 }
 
-int bfs(int n, int m, int **map, int y, int x, int **visitMap, int length) {
+int bfs(int n, int m, int **map, int y, int x, int **visitMap, int length, int **brokenMap) {
 	Queue q;
 	initQueue(&q);
 	
-	enqueue(&q, y, x, length, 0, 0);
-	int breakingX = 0, breakingY = 0;
+	int inputData[4] = {y, x, length, 0};
+	
+	enqueue(&q, inputData);
 	
 	visitMap[y][x] = 1;
+	brokenMap[y][x] = 1;
 	
 	while(!isEmpty(&q)) {
-//		printf("\n===get queue===\n");
+		printf("\n===get queue===\n");
 		int *data = dequeue(&q);
 		int dataY = data[0];
 		int dataX = data[1];
 		int dataLength = data[2];
-		int dataBreakingY = data[3];
-		int dataBreakingX = data[4];
-		visitMap[dataY][dataX] = 1;
+		int isBroken = data[3];
+		printf("Queue Data : %d %d %d %d\nMap: %d\n", dataY, dataX, dataLength, isBroken, map[dataY][dataX]);
 		dataLength++;
 		if(dataY == n - 1 && dataX == m - 1) {
 			isArrived = 1;
@@ -98,18 +86,34 @@ int bfs(int n, int m, int **map, int y, int x, int **visitMap, int length) {
 		for(d=0;d<4;d++) {
 			int ny = dataY + di[d];
 			int nx = dataX + dj[d];
-//			printf("(%d, %d) -----> (%d, %d)\n", dataY, dataX, ny, nx);
-//			printf("DATA(%d, %d)\n", dataBreakingY, dataBreakingX);
-			if(ny < 0 || ny >= n || nx < 0 || nx >= m || visitMap[ny][nx] == 1 || (!(dataBreakingY == 0 && dataBreakingX == 0) && map[ny][nx] == 1) || isArrived) continue;
-			if(map[ny][nx] == 0 || (map[ny][nx] == 1 && (dataBreakingY == 0 && dataBreakingX == 0))) {
-				if(map[ny][nx] == 1) {
-					breakingY = ny;
-					breakingX = nx;
-//					printf("\n===breaking===\n");
+			printf("(%d, %d) -----> (%d, %d)\n", dataY, dataX, ny, nx);
+			if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if((visitMap[ny][nx] == 1 && !isBroken) || (brokenMap[ny][nx] == 1 && isBroken)) continue;
+			if(map[ny][nx] == 0) {
+				printf("ENQUEUE (%d, %d) -----> (%d, %d) length : %d\n", dataY, dataX, ny, nx, dataLength);
+				if(isBroken) {
+					brokenMap[ny][nx] = 1;
+					int newData[4] = {ny, nx, dataLength, isBroken};
+					enqueue(&q, newData);
+				} 
+				if(!isBroken) {
+					visitMap[ny][nx] = 1;
+					int newData[4] = {ny, nx, dataLength, isBroken};
+					enqueue(&q, newData);
 				}
-//				printf("ENQUEUE (%d, %d) -----> (%d, %d) length : %d\n", dataY, dataX, ny, nx, dataLength);
-//				visitMap[ny][nx] = 1;
-				enqueue(&q, ny, nx, dataLength, breakingY, breakingX);
+				
+			}
+			
+			else if(map[ny][nx] == 1 && isBroken == 0) {
+				brokenMap[ny][nx] = 1;
+				printf("ENQUEUE (%d, %d) -----> (%d, %d) length : %d\n", dataY, dataX, ny, nx, dataLength);
+				int newData[4] = {ny, nx, dataLength, 1};
+				enqueue(&q, newData);
+			}
+			
+			else {
+				printf("???\n");
+				printf("Queue Data : %d %d %d %d\nMap: %d\n", ny, nx, dataLength, isBroken, map[ny][nx]);
 			}
 			
 		}
@@ -120,22 +124,20 @@ int main() {
 	int n, m, i, j;
 	
 	scanf("%d %d", &n, &m);
-	int **map = malloc(sizeof(int *) * n);
-	int **visitMap = malloc(sizeof(int *) * n);
+	int map[1001][1001] = {0,};
+	int visitMap[1001][1001] = {0,}; 
+	int brokenMap[1001][1001] = {0,};
 	
-	for(i=0;i<n;i++) {
-		map[i] = malloc(sizeof(int) * m);
-		visitMap[i] = malloc(sizeof(int) * m);
-	}
 	for(i=0;i<n;i++) {
 		for(j=0;j<m;j++) {
 			scanf(" %c", &map[i][j]);
 			visitMap[i][j] = 0;
+			brokenMap[i][j] = 0;
 			map[i][j] -= 48;
 		}
 	}
 	
-	bfs(n, m, map, 0, 0, visitMap, 0);
+	bfs(n, m, map, 0, 0, visitMap, 0, brokenMap);
 	
 	if(!isArrived) printf("-1");
 	else printf("%d", resultLength);
